ca6e9f9deeec1f5d118ab58ab5bb29019490c378
Edidiong Harrison <edidiongharrison18@gmail.com>
2024-05-15 02:48:31 +0100

    new commit

--- app/(root)/dogs/[dogId]/page.tsx
+++ app/(root)/dogs/[dogId]/page.tsx
@@ -1,48 +1,14 @@
-"use client"
-
 import DogGallery from "@/components/DogGallery"
 import DogCard from "@/components/DogCard"
 import DogInfo from "@/components/DogInfo"
 import { getDogDetails, getOtherDogs } from "@/lib/actions/actions"
-import { useEffect, useState } from "react";
-import PreloadAnimation from "../../../../components/PreloadAnimation";
-import "../../preloader.css";
 
 const DogDetails = async ({ params }: { params: { dogId: string }}) => {
   const dogDetails = await getDogDetails(params.dogId)
   const relatedProducts = await getOtherDogs(params.dogId)
 
-  const [isPreloading, setIsPreloading] = useState(true);
-
-  
-  useEffect(() => {
-    setTimeout(() => {
-      setIsPreloading(false);
-    }, 4000);
-
-    const reloadKey = "reloadFlag5";
-    const hasReloaded = localStorage.getItem(reloadKey);
-
-    // Check if the page has already been reloaded
-    if (!hasReloaded) {
-      // Reload the page once after 4 seconds (4000 milliseconds)
-      const timeout = setTimeout(() => {
-        localStorage.setItem(reloadKey, "true"); // Set flag in local storage
-        window.location.reload();
-      }, 4000);
-
-      // Clear the timeout to prevent the reload after the first time
-      return () => clearTimeout(timeout);
-    }
-  }, []);
-
   return (
     <>
-    {isPreloading ? (
-      <PreloadAnimation />
-    ) : (
-    <>
-    
     <div className="flex justify-center items-start gap-16 py-10 px-5 max-md:flex-col max-md:items-center">
       <DogGallery dogMedia={dogDetails.media} />
       <DogInfo dogInfo={dogDetails} />
@@ -57,9 +23,6 @@ const DogDetails = async ({ params }: { params: { dogId: string }}) => {
       </div>
     </div>
     </>
-
-)}
-</>
   )
 }
 
